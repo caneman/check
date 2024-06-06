588d71345c0354dbd400b4d3b0021a84bdf5a525
cane <caneman@163.com>
2024-06-06 11:15:16 +0800

    fix: add proxy

--- auto_burn_tg.py
+++ auto_burn_tg.py
@@ -5,7 +5,7 @@
 # Time   : 2024/6/6 10:20
 """
 cron: 0 30 8 * * ?
-new Env('BurnHair TG自动签到');
+new Env('BurnHair TG签到');
 """
 import re
 import asyncio
@@ -19,23 +19,25 @@ SESSION_NAME = 'telegram'
 CHANNEL_ID = 'burn_hair_bot'
 MSG = '/check'
 API_ID, API_HASH = get_env('tg_api', split=True)
+protocol, ip, port = get_env('tg_proxy', split=True)
+PROXY = (protocol, ip, int(port))
 
 
 async def main():
-    client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
+    client = TelegramClient(SESSION_NAME, API_ID, API_HASH, proxy=PROXY)
 
     @client.on(events.NewMessage(chats=CHANNEL_ID))
     async def handler(event):
         check_res = event.message.text
         count = ''.join(re.findall(r'(\d+)', check_res))
         if count:
-            msg = f'[BurnHair]签到成功，获得「{count}」Token'
+            msg = f'[BurnHair]TG签到成功，获得「{count}」Token'
             success = True
         elif '已经签到' in check_res:
-            msg = f'[BurnHair]已签到'
+            msg = f'[BurnHair]TG已签到'
             success = True
         else:
-            msg = f'[BurnHair]签到失败，{check_res}'
+            msg = f'[BurnHair]TG签到失败，{check_res}'
             success = False
 
         if success is False:
